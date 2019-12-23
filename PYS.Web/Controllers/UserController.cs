ecf5da39cc1a89be14f76a7c23c5370e301f8a1a
merveytu <mervekose2009@gmail.com>
2019-12-23 22:15:26 +0300

    Personel Listesi Ekranı Eklendi. Model gösterimi yapıldı. Tablo örneği tekrarlandı.

---
+++ PYS.Web/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using PYS.Web.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PYS.Web.Controllers
+{
+    public class UserController : Controller
+    {        
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Kullanıcı Listesi";
+
+
+            var users = GenerateUsers();           
+
+            return View(users);
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.Title = "Kullanıcılar";
+            var users = GenerateUsers();
+            return View(users);
+        }
+
+
+        private static List<UserDto> GenerateUsers()
+        {
+            var users = new List<UserDto>();
+            users.Add(new UserDto() { Name = "Merve", Surname = "Köse", Age = 20, Description = "Bu veriler test amaçlı eklenmiştir." });
+            users.Add(new UserDto() { Name = "Merve 2", Surname = "Köse 2", Age = 22, Description = "Bu veriler test amaçlı eklenmiştir. 2" });
+            users.Add(new UserDto() { Name = "Merve 3", Surname = "Köse 3", Age = 25, Description = "Bu veriler test amaçlı eklenmiştir. 3" });
+            users.Add(new UserDto() { Name = "Merve 4", Surname = "Köse 4", Age = 24, Description = "Bu veriler test amaçlı eklenmiştir. 4" });
+            users.Add(new UserDto() { Name = "Merve 5", Surname = "Köse 5", Age = 26, Description = "Bu veriler test amaçlı eklenmiştir. 5 " });
+            users.Add(new UserDto() { Name = "Merve 6", Surname = "Köse 6", Age = 26, Description = "Bu veriler test amaçlı eklenmiştir. 6 " });
+            return users;
+        }
+    }
+}
---
+++ PYS.Web/Models/User/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PYS.Web.Models.User
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? Age { get; set; }
+        public string Description { get; set; }
+    }
+
+}
