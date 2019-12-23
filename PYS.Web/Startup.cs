162e9518d62228452090a9ede34ad22d6bcdfba1
merveytu <mervekose2009@gmail.com>
2019-12-23 20:57:47 +0300

    Mvc projesi oluşturuldu.

---
+++ PYS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PYS.Web.Startup))]
+namespace PYS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
